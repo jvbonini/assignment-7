3f3bd37a68e84eb64b88b4a604d692a4ce2d409e
Joao Victor Bonini <43345710+jvbonini@users.noreply.github.com>
2019-04-08 18:41:15 -0500

    Cleaning for deliver

--- arquive/sakila_queries_v1.sql
+++ arquive/sakila_queries_v1.sql
@@ -109,13 +109,14 @@ GROUP BY first_name, last_name
 ORDER BY last_name; 
 
 -- Part 7 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx --
+
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. 
 -- As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
 -- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
 SELECT title
 FROM film
 WHERE title 
-LIKE 'K%' OR title LIKE 'Q%' 
+HAVING 'K%' OR title HAVING 'Q%' 
 AND title IN
 	(
 	SELECT title
